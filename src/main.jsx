af37a7e3e53b05506d3a823ed8948ac612b37875
Arthur Bobadilla <arthurbobadilla2006@gmail.com>
2024-11-16 19:30:06 -0300

    removed car.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App.jsx'
-import Home from './routes/Home.jsx'
-import Login from './routes/Login.jsx'
-import Error from './routes/Error.jsx'
-import Car from './routes/Car.jsx'
-import Eolica from './routes/Eolica.jsx'
-import Solar from './routes/Solar.jsx'
-import Hidraulica from './routes/Hidraulica.jsx'
-import Nuclear from './routes/Nuclear.jsx'
-import Selection from './routes/Selection.jsx'
-
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import Home from "./routes/Home.jsx";
+import Login from "./routes/Login.jsx";
+import Error from "./routes/Error.jsx";
+import Eolica from "./routes/Eolica.jsx";
+import Solar from "./routes/Solar.jsx";
+import Hidraulica from "./routes/Hidraulica.jsx";
+import Nuclear from "./routes/Nuclear.jsx";
+import Selection from "./routes/Selection.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/', element: <App />,
+    path: "/",
+    element: <App />,
     errorElement: <Error />,
 
     children: [
-      { path: '/', element: <Home /> },
-      { path: '/login', element: <Login /> },
-      { path: '/car', element: <Car /> },
-      { path: '/energiaeolica', element: <Eolica/>},
-      { path: '/energiasolar', element: <Solar/>},
-      { path: '/energiahidraulica', element: <Hidraulica/>},
-      { path: '/energianuclear', element: <Nuclear/>},
-      { path: '/selection', element: <Selection /> },
-    ]
-  }
-])
+      { path: "/", element: <Home /> },
+      { path: "/login", element: <Login /> },
+      { path: "/energiaeolica", element: <Eolica /> },
+      { path: "/energiasolar", element: <Solar /> },
+      { path: "/energiahidraulica", element: <Hidraulica /> },
+      { path: "/energianuclear", element: <Nuclear /> },
+      { path: "/selection", element: <Selection /> },
+    ],
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
